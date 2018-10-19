bf711960448d030c16b4cf8737d76105b3354fa8
Mohammed Shaik Hussain Ali <me@shaiksphre.com>
2018-10-18 23:17:57 -0700

    November 2018

--- content/waiting/resources/js/index.js
+++ content/waiting/resources/js/index.js
@@ -88,7 +88,8 @@ const DATA_SET = {
         July: "03/15/2009",
         August: "03/15/2009",
         September: "01/01/2007",
-        October: "03/26/2009"
+        October: "03/26/2009",
+        November: "03/26/2009"
     }
 };
 
