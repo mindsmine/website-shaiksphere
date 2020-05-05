d20728b2235e6ec3551310798d6fcccfb88cb9a1
Mohammed Shaik Hussain Ali <me@shaiksphre.com>
2020-05-04 22:40:44 -0400

    Updating for May

--- content/waiting/resources/js/index.js
+++ content/waiting/resources/js/index.js
@@ -110,15 +110,16 @@ const DATA_SET = {
         January: "05/18/2009",
         February: "05/19/2009",
         March: "05/22/2009",
-        April: "05/25/2009"
+        April: "05/25/2009",
+        May: "06/02/2009"
     }
 };
 
 const EARLIER_PRIORITY_DATE_MOMENT = moment(DATA_SET["2019"].August, DATE_FORMAT);
-const LATEST_PRIORITY_DATE_MOMENT = moment(DATA_SET["2020"].April, DATE_FORMAT);
+const LATEST_PRIORITY_DATE_MOMENT = moment(DATA_SET["2020"].May, DATE_FORMAT);
 
 // When was the last time the date changed
-const MOVEMENT_MONTHS = 8;
+const MOVEMENT_MONTHS = 9;
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
