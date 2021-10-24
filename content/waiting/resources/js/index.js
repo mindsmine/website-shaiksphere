7d15c871e4fcd9751fae81d2e2c2936d31e9f296
Mohammed Shaik Hussain Ali <me@shaiksphere.com>
2021-10-24 01:31:12 -0400

    Update November

--- content/waiting/resources/js/index.js
+++ content/waiting/resources/js/index.js
@@ -131,12 +131,14 @@ const DATA_SET = {
         June: "12/01/2010",
         July: "06/01/2011",
         August: "06/01/2011",
-        September: "09/01/2011"
+        September: "09/01/2011",
+        October: "09/01/2011",
+        November: "12/01/2011"
     }
 };
 
-const EARLIER_PRIORITY_DATE = dayjs(DATA_SET["2020"].September, DATE_FORMAT);
-const LATEST_PRIORITY_DATE = dayjs(DATA_SET["2021"].September, DATE_FORMAT);
+const EARLIER_PRIORITY_DATE = dayjs(DATA_SET["2020"].November, DATE_FORMAT);
+const LATEST_PRIORITY_DATE = dayjs(DATA_SET["2021"].November, DATE_FORMAT);
 
 // When was the last time the date changed
 const MOVEMENT_MONTHS = 12;
