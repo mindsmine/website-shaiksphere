caca0ac789c8e4fcc7e1704319feae5e1d5d7e4e
Mohammed Shaik Hussain Ali <me@shaiksphere.com>
2022-08-18 00:15:08 -0400

    September update

--- content/waiting/resources/js/index.js
+++ content/waiting/resources/js/index.js
@@ -144,11 +144,12 @@ const DATA_SET = {
         May: "09/01/2013",
         June: "09/01/2014",
         July: "12/01/2014",
-        August: "12/01/2014"
+        August: "12/01/2014",
+        September: "12/01/2014"
     }
 };
 
-const LATEST_PRIORITY_DATE = dayjs(DATA_SET["2022"].August, DATE_FORMAT);
+const LATEST_PRIORITY_DATE = dayjs(DATA_SET["2022"].September, DATE_FORMAT);
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
