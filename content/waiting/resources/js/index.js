13954663231a5fb8298e2b1af89a67fe80f0834a
Mohammed Shaik Hussain Ali <me@shaiksphere.com>
2024-07-10 04:54:59 -0400

    Adding August bulletin

--- content/waiting/resources/js/index.js
+++ content/waiting/resources/js/index.js
@@ -171,11 +171,12 @@ const DATA_SET = {
         April: "04/01/2012",
         May: "04/15/2012",
         June: "04/15/2012",
-        July: "06/15/2012"
+        July: "06/15/2012",
+        August: "07/15/2012"
     }
 };
 
-const LATEST_PRIORITY_DATE = dayjs(DATA_SET["2024"].July, DATE_FORMAT);
+const LATEST_PRIORITY_DATE = dayjs(DATA_SET["2024"].August, DATE_FORMAT);
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
