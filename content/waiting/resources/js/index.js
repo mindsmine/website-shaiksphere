5069b15b9fcaef2bbfc978badd300b45bbe8917d
Mohammed Shaik Hussain Ali <me@shaiksphere.com>
2025-03-10 23:18:23 -0400

    April Bulletin

--- content/waiting/resources/js/index.js
+++ content/waiting/resources/js/index.js
@@ -181,11 +181,12 @@ const DATA_SET = {
     2025: {
         January: "10/01/2012",
         February: "10/15/2012",
-        March: "12/01/2012"
+        March: "12/01/2012",
+        April: "01/01/2013"
     }
 };
 
-const LATEST_PRIORITY_DATE = dayjs(DATA_SET["2025"].March, DATE_FORMAT);
+const LATEST_PRIORITY_DATE = dayjs(DATA_SET["2025"].April, DATE_FORMAT);
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
