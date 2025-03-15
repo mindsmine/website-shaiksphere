40fb00ba154d3adb94a97f2786dab5f601ae4e2f
Mohammed Shaik Hussain Ali <me@shaiksphere.com>
2025-03-15 10:24:41 -0400

    Adding ayat-ul-kursi

--- content/quran-portions/resources/js/index.js
+++ content/quran-portions/resources/js/index.js
@@ -27,7 +27,12 @@ const SurahList = [];
 
 SurahList.push(new SurahHolder(  1,  7, "al-Fātiḥah", "The Opening"));
 SurahList.push(null);
-SurahList.push(new SurahHolder(  2,  6, "al-Baq̈arah", "The Cow"));
+SurahList.push(null);
+SurahList.push(new SurahHolder(  2,  6, "al-Baq̈arah", "The Cow (<em>5 verses</em>)"));
+SurahList.push(null);
+SurahList.push(null);
+SurahList.push(new SurahHolder(  2, 256, "Ayāh al-Kursī", "Verse of the Throne"));
+SurahList.push(null);
 SurahList.push(null);
 SurahList.push(new SurahHolder(103,  4, "al-'Aṣr", "The Declining Day"));
 SurahList.push(new SurahHolder(104, 10, "al-Humazah", "The Gossipmonger"));
@@ -50,11 +55,17 @@ const onDOMLoad = () => {
             tbodyContent.push(`<tr><td><hr /></td></tr>`);
         } else {
             tbodyContent.push(`<tr><td class="surahTitle">${surah.index} - ${surah.name} - ${surah.translation}</td></tr>`);
-    
-            for (let i = 1; i <= surah.count; i++) {
-                let imgName = `${Number(surah.index).toString().padStart(3, 0)}-${Number(i).toString().padStart(3, 0)}`;
-    
-                tbodyContent.push(`<tr><td><img src="https://www.alislam.org/quran/search2/verses/${imgName}.png" /></td></tr>`);
+
+            if (surah.count === 256) {
+                tbodyContent.push(`<tr><td><img src="https://www.alislam.org/quran/search2/verses/002-001.png" /></td></tr>`);
+                tbodyContent.push(`<tr><td><img src="https://www.alislam.org/quran/search2/verses/002-256a.png" /></td></tr>`);
+                tbodyContent.push(`<tr><td><img src="https://www.alislam.org/quran/search2/verses/002-256b.png" /></td></tr>`);
+            } else {
+                for (let i = 1; i <= surah.count; i++) {
+                    let imgName = `${Number(surah.index).toString().padStart(3, 0)}-${Number(i).toString().padStart(3, 0)}`;
+        
+                    tbodyContent.push(`<tr><td><img src="https://www.alislam.org/quran/search2/verses/${imgName}.png" /></td></tr>`);
+                }
             }
     
             tbodyContent.push(`<tr><td>&nbsp;</td></tr>`);
